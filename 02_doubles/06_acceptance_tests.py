d499f74d49fa55255770c46577cd1d828ca4f051
Alessandro Molina <amol@turbogears.org>
2020-03-26 23:50:25 +0100

    done

--- 02_doubles/06_acceptance_tests.py
+++ 02_doubles/06_acceptance_tests.py
@@ -128,5 +128,37 @@ def new_chat_server():
     return _ChatServerManager(("", 9090), authkey=b'mychatsecret')
 
 
+class FakeServer:
+    def __init__(self):
+        self.last_command = None
+        self.messages = []
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def send(self, data):
+        callid, command, args, kwargs = data
+        self.last_command = command
+        self.last_args = args
+
+    def recv(self, *args, **kwargs):
+        if self.last_command == "dummy":
+            return "#RETURN", None
+        elif self.last_command == "create":
+            return "#RETURN", ("fakeid", tuple())
+        elif self.last_command == "append":
+            self.messages.append(self.last_args[0])
+            return "#RETURN", None
+        elif self.last_command == "__getitem__":
+            return "#RETURN", self.messages[self.last_args[0]]
+        elif self.last_command in ("incref", "decref", "accept_connection"):
+            return "#RETURN", None
+        else:
+            return "#ERROR", ValueError("%s - %r" % (self.last_command, self.last_args))
+
+    def close(self):
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
--- 02_doubles/07_dependency_injection.py
+++ 02_doubles/07_dependency_injection.py
@@ -21,28 +21,26 @@ class TestChatClient(unittest.TestCase):
         assert client.nickname == "User 1"
 
     def test_send_message(self):
-        client = ChatClient("User 1")
-        client.connection = unittest.mock.Mock()
+        client = ChatClient("User 1", connection_provider=unittest.mock.Mock())
 
         sent_message = client.send_message("Hello World")
         
         assert sent_message == "User 1: Hello World"
 
     def test_client_connection(self):
-        client = ChatClient("User 1")
-
         connection_spy = unittest.mock.MagicMock()
-        with unittest.mock.patch.object(client, "_get_connection", 
-                                        return_value=connection_spy):
-            client.send_message("Hello World")
+
+        client = ChatClient("User 1", connection_provider=lambda *args: connection_spy)
+        client.send_message("Hello World")
 
         connection_spy.broadcast.assert_called_with(("User 1: Hello World"))
 
     def test_client_fetch_messages(self):
-        client = ChatClient("User 1")
-        client.connection = unittest.mock.Mock()
-        client.connection.get_messages.return_value = ["message1", "message2"]
+        connection = unittest.mock.Mock()
+        connection.get_messages.return_value = ["message1", "message2"]
 
+        client = ChatClient("User 1", connection_provider=lambda *args: connection)
+        
         starting_messages = client.fetch_messages()
         client.connection.get_messages().append("message3")
         new_messages = client.fetch_messages()
@@ -74,10 +72,23 @@ class TestConnection(unittest.TestCase):
             assert c2.get_messages()[-1] == "connected message"
 
 
+from multiprocessing.managers import SyncManager, ListProxy
+class Connection(SyncManager):
+    def __init__(self, address):
+        self.register("get_messages", proxytype=ListProxy)
+        super().__init__(address=address, authkey=b'mychatsecret')
+        self.connect()
+
+    def broadcast(self, message):
+        messages = self.get_messages()
+        messages.append(message)
+
+
 class ChatClient:
-    def __init__(self, nickname):
+    def __init__(self, nickname, connection_provider=Connection):
         self.nickname = nickname
         self._connection = None
+        self._connection_provider = connection_provider
         self._last_msg_idx = 0
 
     def send_message(self, message):
@@ -94,29 +105,8 @@ class ChatClient:
     @property
     def connection(self):
         if self._connection is None:
-            self._connection = self._get_connection()
+            self._connection = self._connection_provider(("localhost", 9090))
         return self._connection
-
-    @connection.setter
-    def connection(self, value):
-        if self._connection is not None:
-            self._connection.close()
-        self._connection = value
-
-    def _get_connection(self):
-        return Connection(("localhost", 9090))
-
-
-from multiprocessing.managers import SyncManager, ListProxy
-class Connection(SyncManager):
-    def __init__(self, address):
-        self.register("get_messages", proxytype=ListProxy)
-        super().__init__(address=address, authkey=b'mychatsecret')
-        self.connect()
-
-    def broadcast(self, message):
-        messages = self.get_messages()
-        messages.append(message)
 
 
 def new_chat_server():
@@ -126,6 +116,38 @@ def new_chat_server():
     _ChatServerManager.register("get_messages", callable=lambda: messages, 
                                 proxytype=ListProxy)    
     return _ChatServerManager(("", 9090), authkey=b'mychatsecret')
+
+
+class FakeServer:
+    def __init__(self):
+        self.last_command = None
+        self.messages = []
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def send(self, data):
+        callid, command, args, kwargs = data
+        self.last_command = command
+        self.last_args = args
+
+    def recv(self, *args, **kwargs):
+        if self.last_command == "dummy":
+            return "#RETURN", None
+        elif self.last_command == "create":
+            return "#RETURN", ("fakeid", tuple())
+        elif self.last_command == "append":
+            self.messages.append(self.last_args[0])
+            return "#RETURN", None
+        elif self.last_command == "__getitem__":
+            return "#RETURN", self.messages[self.last_args[0]]
+        elif self.last_command in ("incref", "decref", "accept_connection"):
+            return "#RETURN", None
+        else:
+            return "#ERROR", ValueError("%s - %r" % (self.last_command, self.last_args))
+
+    def close(self):
+        pass
 
 
 if __name__ == '__main__':
